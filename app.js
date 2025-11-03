065f7b7db772b7a8e34fe31b04a2a8423777e94e
melkordev <bahamondejosepablo@gmail.com>
2025-11-03 12:09:26 -0300

    feat: Agregar logger categorizado

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ function logMessage(message) {
   console.log(`[LOG] ${message}`);
 }
 
+// funcion de log categorizado
+const Logger = {
+  info: (msg) => console.log(`[INFO] ${new Date().toISOString()} - ${msg}`),
+  warn: (msg) => console.warn(`[WARN] ${new Date().toISOString()} - ${msg}`),
+  error: (msg) => console.error(`[ERROR] ${new Date().toISOString()} - ${msg}`),
+  debug: (msg) => console.debug(`[DEBUG] ${new Date().toISOString()} - ${msg}`)
+};
+
